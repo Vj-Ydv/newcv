0989455e5f1a1eb18832494aa8dd1b40620d4f54
vijay yadav <vizayyadav123@gmail.com>
2019-05-11 12:20:41 +0545

    CV preview file added

--- src/newcv/newcv/urls.py
+++ src/newcv/newcv/urls.py
@@ -13,9 +13,29 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
+
 from django.contrib import admin
 from django.urls import path
+from curriculum.views import export_single_page, export_classic, export_class,export_classic1,addlanguage,addlanguageitem,addresume, addskill, addskillitem, addcertification, addcertificationitem, addexperience, addproject, addprojectitem, addtraining, menulist
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    path('classic/',export_classic,name='classic'),
+    path('classic1/',export_classic1,name='classic1'),
+    path('modern/',export_single_page,name='modern'),
+    path('htmltest/<str:first_name>/',export_class,name='test'),
+    path('language/',addlanguage,name='language'),
+    path('languageitem/',addlanguageitem,name='languageitem'),
+    path('resume/',addresume,name='resume'),
+    path('skill/',addskill,name='skill'),
+    path('skillitem/',addskillitem,name='skillitem'),
+    path('certification/',addcertification,name='certification'),
+    path('certificationitem/',addcertificationitem,name='certificationitem'),
+    path('experience/',addexperience,name='experience'),
+    path('project/',addproject,name='project'),
+    path('projectitem/',addprojectitem,name='projectitem'),
+    path('training/',addtraining,name='training'),
+    path('',menulist,name='showmenu'),
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
